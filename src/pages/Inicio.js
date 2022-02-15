49710b7da9b187c512035435961e4c9d68b0b57e
Sophiae <sophia.makita@revalcodesolution.comm>
2022-02-15 17:47:48 -0300

    activities

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,10 +1,25 @@
-import { useRef, useEffect} from 'react';
-import { Power3, TweenMax } from 'gsap';
+import NuestrasActividades from './NuestrasActividades';
 
 function Inicio() {
     return (
-          <div className="container main">
-              <h1>Inicio</h1>
+          <div className="main">
+              {/* make an alternative image that is only viewed in mobile and hide this one so it is only visible for tablet and pc */}
+              <img alt='banner' className='img-fluid' src={require('../img/banner.png')}/>
+              <div className='container padTop'>
+                <h3>Quien Somos</h3>
+                <div className='row'>
+                    <div className='col-8'>
+                        <p className='quienText'>Laboris sint laboris Lorem et elit occaecat ut veniam aliqua non. Ullamco ullamco ea labore in. Sit non quis minim pariatur sint.
+
+                            Id ullamco reprehenderit ea nostrud commodo magna irure deserunt duis ipsum reprehenderit labore. Duis consequat nulla veniam voluptate aute. Deserunt est aliqua tempor deserunt eu mollit aliqua ipsum est minim nulla incididunt voluptate. Ullamco duis elit ut in pariatur aliqua ullamco esse dolor mollit id eu esse irure. Dolore amet reprehenderit commodo commodo nulla anim est reprehenderit veniam minim qui excepteur aute velit. Cupidatat in pariatur laborum nulla cupidatat labore occaecat pariatur ullamco aliquip et labore aliqua.</p>
+                    </div>
+                    
+                    <div className='col-4'>
+                        <img alt='quien somos' className='quienSomos' src={require('../img/logo.png')} />
+                    </div>
+                    <NuestrasActividades/>
+                </div>
+              </div>
           </div>
       );
   }
--- src/pages/NuestrasActividades.js
+++ src/pages/NuestrasActividades.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Actividad(props) {
+  return (
+    <div className='row padTop'>
+
+      <div className='col-3'>
+          <img alt={props.actividad} src={props.img}/>
+      </div>
+      
+      <div className='col-9'>
+          <h4>{props.actividad}</h4>
+          <p>{props.descripcion}</p>
+          <Link to={props.to}>
+            <img className='moreInfo' alt='mas info' src={require('../img/mas_info.png')}/>
+          </Link>
+      </div>
+
+    </div>
+  )
+}
+
+
+// Conectar esta parte de la app a firebase database para poder actualizar los recursos usados facilmente y sin necesidad de meterme al codigo
 
 function NuestrasActividades() {
   return (
-    <div>
-        NuestrasActividades
-    </div>
+      <div className='container padTop activities padBot'>
+          <h3>Nuestras Actividades</h3>
+
+          <Actividad
+            actividad='Somos Suyas'
+            img={require('../img/activities.png')}
+            descripcion='Minim laboris ex do irure amet nulla laboris non. Amet consectetur aute eu nulla aliquip ad. Amet sunt excepteur nostrud tempor mollit laboris. Anim commodo officia excepteur laboris do ea. Minim enim cupidatat ea deserunt mollit exercitation elit quis. Nulla adipisicing tempor laborum enim eu enim.'
+            to='/somossuyas'
+          />
+
+          <Actividad
+            actividad='A tu Alcance'
+            img={require('../img/activities.png')}
+            descripcion='Minim laboris ex do irure amet nulla laboris non. Amet consectetur aute eu nulla aliquip ad. Amet sunt excepteur nostrud tempor mollit laboris. Anim commodo officia excepteur laboris do ea. Minim enim cupidatat ea deserunt mollit exercitation elit quis. Nulla adipisicing tempor laborum enim eu enim.'
+            to='/talleres'
+          />
+
+          <Actividad
+            actividad='Escuela'
+            img={require('../img/activities.png')}
+            descripcion='Minim laboris ex do irure amet nulla laboris non. Amet consectetur aute eu nulla aliquip ad. Amet sunt excepteur nostrud tempor mollit laboris. Anim commodo officia excepteur laboris do ea. Minim enim cupidatat ea deserunt mollit exercitation elit quis. Nulla adipisicing tempor laborum enim eu enim.'
+            to='/escuela'
+          />
+
+          <Actividad
+            actividad='Mejor es Dar'
+            img={require('../img/activities.png')}
+            descripcion='Minim laboris ex do irure amet nulla laboris non. Amet consectetur aute eu nulla aliquip ad. Amet sunt excepteur nostrud tempor mollit laboris. Anim commodo officia excepteur laboris do ea. Minim enim cupidatat ea deserunt mollit exercitation elit quis. Nulla adipisicing tempor laborum enim eu enim.'
+            to='/mejoresdar'
+          />
+
+      </div>
   );
 }
 
-export default NuestrasActividades
+export default NuestrasActividades;
